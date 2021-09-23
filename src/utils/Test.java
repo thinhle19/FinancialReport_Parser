c991429f8e23b8990174b8059352e515eaac927d
Thinh Le <letienthinh0109@gmail.com>
2021-09-23 17:39:59 +0700

    Finish all main functions & update .gitignore

--- src/utils/Test.java
+++ src/utils/Test.java
@@ -4,21 +4,12 @@
  * and open the template in the editor.
  */
 package utils;
-
-<<<<<<< HEAD
-import java.io.File;
 import java.io.FileNotFoundException;
 import models.ParseManager;
 //import java.time.YearMonth;
 //import java.time.format.DateTimeFormatter;
-=======
-import constants.Constant;
-
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
-import java.util.TreeMap;
-
->>>>>>> 221a46e (Gradle but not understand anything, I'm gonna roll back)
 import models.Report;
 
 /**
@@ -28,14 +19,13 @@ public class Test {
 
     public static void main(String[] args) throws FileNotFoundException {
         //yearmonth parser test
-<<<<<<< HEAD
 //        YearMonth ym = YearMonth.parse("122015", DateTimeFormatter.ofPattern("MMyyyy"));
 //        System.out.println(ym.getMonth() + " " + ym.getYear());
 //        System.out.println(ym.format(DateTimeFormatter.ofPattern("MMyyyy")));
 //        Report r = new Report("PLP", "122015", "PLP_122015.xlsx");
 //        System.out.println(r.dataPath);
 //        System.out.println(r.detailData.keySet());
-        XLSXParser.parseToMap("D:\\Programming\\#Java\\Projects\\FinancialAutoReport\\financial_data\\PLP\\csv\\PLP_122020.xlsx");
+//        XLSXParser.parseToMap("D:\\Programming\\#Java\\Projects\\FinancialAutoReport\\financial_data\\PLP\\csv\\PLP_122020.xlsx");
         ParseManager pm = new ParseManager();
         pm.writeAllCompanyReport();
         pm.writeDataToMainFile("D:\\Programming\\#Java\\Projects\\FinancialAutoReport\\financial_data\\result.xlsx");
@@ -44,11 +34,4 @@ public class Test {
 //Validator test
 //        System.out.println(Validator.trimParentheses("(hihifef.)"));
 //        System.out.println(JSONParser.parseToMap(""));
-=======
-        YearMonth ym = YearMonth.parse("122015", DateTimeFormatter.ofPattern("MMyyyy"));
-        System.out.println(ym.getMonth() + " " + ym.getYear());
-        System.out.println(ym.format(DateTimeFormatter.ofPattern("MMyyyy")));
-        Report r = new Report("PLP", "122015");
-    }
->>>>>>> 221a46e (Gradle but not understand anything, I'm gonna roll back)
 }
